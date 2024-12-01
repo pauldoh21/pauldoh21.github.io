f33d2e91c6688321c9af66cd3060bd46ded75dd2
Paul <2566465D@student.gla.ac.uk>
2024-12-01 00:49:14 +0000

    Can now use divs in posts

--- my-portfolio/src/components/Post.js
+++ my-portfolio/src/components/Post.js
@@ -20,66 +20,84 @@ function Post({ posts }) {
   if (!post) return <p>Post not found</p>;
 
   // Helper function to render content blocks
-  const renderContent = (contentArray) => {
-    return contentArray.map((block, index) => {
-      switch (block.type) {
-        case 'h2':
-          return <h2 key={index}>{block.content}</h2>;
+const renderContent = (contentArray) => {
+  return contentArray.map((block, index) => {
+    switch (block.type) {
+      case 'h2':
+        return <h2 key={index}>{block.content}</h2>;
 
-        case 'p':
-          return (
-            <p key={index}>
-              {block.content.map((item, i) => {
-                if (item.type === 'text') {
-                  return item.value.split('\n').map((line, j) => (
-                    <React.Fragment key={`${i}-${j}`}>
-                      {line}
-                      {j < item.value.split('\n').length - 1 && <br />}
-                    </React.Fragment>
-                  ));
-                } else if (item.type === 'link') {
-                  return (
-                    <a 
-                      key={i} 
-                      href={item.href} 
-                      target={item.newTab ? "_blank" : undefined} 
-                      rel={item.newTab ? "noopener noreferrer" : undefined}
-                    >
-                      {item.value}
-                    </a>
-                  );
-                }
-                return null;
-              })}
-            </p>
-          );
+      case 'p':
+        return (
+          <p key={index}>
+            {block.content.map((item, i) => {
+              if (item.type === 'text') {
+                return item.value.split('\n').map((line, j) => (
+                  <React.Fragment key={`${i}-${j}`}>
+                    {line}
+                    {j < item.value.split('\n').length - 1 && <br />}
+                  </React.Fragment>
+                ));
+              } else if (item.type === 'link') {
+                return (
+                  <a 
+                    key={i} 
+                    href={item.href} 
+                    target={item.newTab ? "_blank" : undefined} 
+                    rel={item.newTab ? "noopener noreferrer" : undefined}
+                  >
+                    {item.value}
+                  </a>
+                );
+              }
+              return null;
+            })}
+          </p>
+        );
 
-        case 'image':
-          return (
-            <img
-              key={index}
-              src={block.src}
-              alt={block.alt}
-              style={block.style || {}}
-            />
-          );
+      case 'ul':
+        return (
+          <ul key={index}>
+            {block.content.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </ul>
+        );
 
-        case 'Gallery':
-          return (
-            <div style={block.style || {}}>
-              <Gallery>
-                {block.images.map((image, i) => (
-                  <img key={i} src={image.src} alt={image.alt} />
-                ))}
-              </Gallery>
-            </div>
-          );
+      case 'image':
+        return (
+          <img
+            key={index}
+            src={block.src}
+            alt={block.alt}
+            style={block.style || {}}
+          />
+        );
 
-        default:
-          return null;
-      }
-    });
-  };
+      case 'Gallery':
+        return (
+          <div key={index} style={block.style || {}}>
+            <Gallery>
+              {block.images.map((image, i) => (
+                <img key={i} src={image.src} alt={image.alt} />
+              ))}
+            </Gallery>
+          </div>
+        );
+
+      // New 'div' case to handle nested blocks
+      case 'div':
+        return (
+          <div key={index} style={block.style || {}}>
+            {renderContent(block.content)}  {/* Recursive call */}
+          </div>
+        );
+
+      default:
+        return null;
+    }
+  });
+};
+
 
   return (
     <div id="main" className="post-container">
